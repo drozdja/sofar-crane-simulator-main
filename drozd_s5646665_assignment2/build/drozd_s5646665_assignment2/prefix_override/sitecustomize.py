d85233f338a759cf9a61fea58728700c7021df6a
drozdja <drozdja@gmail.com>
2025-02-03 23:25:42 +0100

    gitignore update

--- drozd_s5646665_assignment2/build/drozd_s5646665_assignment2/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/drozdja/Desktop/sofar2/sofar-crane-simulator-main/drozd_s5646665_assignment2/install/drozd_s5646665_assignment2'
